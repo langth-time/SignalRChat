462b253121ff707cdce8f6d3660729d9d6aea15f
Ethan <1224710380@qq.com>
2019-11-04 17:14:08 +0800

    add annotation

--- SignalRChat/SignalRChat/Hubs/ChatHub.cs
+++ SignalRChat/SignalRChat/Hubs/ChatHub.cs
@@ -5,6 +5,12 @@ namespace SignalRChat.Hubs
 {
     public class ChatHub : Hub
     {
+        /// <summary>
+        /// 发送消息给所有用户
+        /// </summary>
+        /// <param name="user">发送者</param>
+        /// <param name="message">发送消息</param>
+        /// <returns></returns>
         public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
