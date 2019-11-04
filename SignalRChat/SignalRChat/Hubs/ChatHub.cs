f349071fd65fc6df18f7324bf6171ae5aa6e095d
Ethan <1224710380@qq.com>
2019-11-04 17:20:08 +0800

    1

--- SignalRChat/SignalRChat/Hubs/ChatHub.cs
+++ SignalRChat/SignalRChat/Hubs/ChatHub.cs
@@ -14,6 +14,7 @@ namespace SignalRChat.Hubs
         public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
+
         }
     }
 }
